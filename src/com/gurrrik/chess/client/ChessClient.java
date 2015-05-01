0857c9285685b2ae7f5b66bdba73d161f6f0ed79
Anton Guryanov <guryanov91@gmail.com>
2015-05-01 22:36:51 +0300

    Started implementing CLI client (parse input lines in a separate method).

--- src/com/gurrrik/chess/client/ChessClient.java
+++ src/com/gurrrik/chess/client/ChessClient.java
@@ -6,7 +6,6 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.Channel;
-import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -21,6 +20,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 class CLArgs {
     @Parameter
@@ -34,6 +35,46 @@ class CLArgs {
 }
 
 public class ChessClient {
+    private void handleInput(Channel ch) throws Exception {
+        Pattern startGamePattern = Pattern.compile("^start\\s+(\\d+)$", Pattern.CASE_INSENSITIVE);
+        Pattern movePattern = Pattern.compile("^move\\s+(\\w+)", Pattern.CASE_INSENSITIVE);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        for (String line = in.readLine(); line != null; line = in.readLine()) {
+            Matcher startGameMatch = startGamePattern.matcher(line);
+            if (startGameMatch.matches()) {
+                long gameId;
+                try {
+                    gameId = Integer.parseInt(startGameMatch.group(1));
+                } catch (NumberFormatException e) {
+                    System.err.println(e.toString());
+                    continue;
+                }
+
+                Messages.MClientMessage.MStartGame.Builder msgStartGameBuilder
+                        = Messages.MClientMessage.MStartGame.newBuilder();
+                msgStartGameBuilder.setGameId(gameId);
+
+                Messages.MClientMessage.Builder msgBuilder
+                        = Messages.MClientMessage.newBuilder();
+                msgBuilder.setType(Messages.MClientMessage.EType.START_GAME);
+                msgBuilder.setStartGame(msgStartGameBuilder.build());
+
+                Messages.MClientMessage msg = msgBuilder.build();
+                ch.writeAndFlush(msg).sync();
+
+                continue;
+            }
+
+            Matcher moveMatch = movePattern.matcher(line);
+            if (moveMatch.matches()) {
+                continue;
+            }
+
+            System.err.println("Unrecognized command");
+        }
+    }
+
     public void startClient(String server, int port) throws Exception {
         EventLoopGroup group = new NioEventLoopGroup();
 
@@ -53,32 +94,7 @@ public class ChessClient {
                         }
                     });
             Channel ch = b.connect(server, port).sync().channel();
-
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            for (;;) {
-                String line = in.readLine();
-                if (line == null)
-                    break;
-
-                long gameId;
-                try {
-                    gameId = Long.parseLong(line);
-                } catch (NumberFormatException e) {
-                    continue;
-                }
-
-                Messages.MClientMessage.MStartGame.Builder msgStartGameBuilder
-                        = Messages.MClientMessage.MStartGame.newBuilder();
-                msgStartGameBuilder.setGameId(gameId);
-
-                Messages.MClientMessage.Builder msgBuilder
-                        = Messages.MClientMessage.newBuilder();
-                msgBuilder.setType(Messages.MClientMessage.EType.START_GAME);
-                msgBuilder.setStartGame(msgStartGameBuilder.build());
-
-                Messages.MClientMessage msg = msgBuilder.build();
-                ch.writeAndFlush(msg).sync();
-            }
+            handleInput(ch);
         } finally {
             group.shutdownGracefully();
         }
